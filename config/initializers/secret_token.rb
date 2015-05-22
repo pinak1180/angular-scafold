c8c057e76f58f0a7307d20f0c653806ecc95a2d8
pinak <pinak.0605@gmail.com>
2015-05-22 16:53:16 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularScaffold::Application.config.secret_key_base = '982ae82040e416d7443e2b396d55a5377746c239f338429519195e2329d8da1abab9d8cb41b5456b1c1b2f2c5cefb2df5b1b343606acbfc773ab26a235e963bb'
